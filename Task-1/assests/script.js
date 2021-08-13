e310c3f886719312f22ebb97e6dc2fb797bc5edb
Jigar Sable <jigarsable21@gmail.com>
2021-08-13 21:55:40 +0530

    video project added

--- Task-1/assests/script.js
+++ Task-1/assests/script.js
@@ -34,10 +34,35 @@ slideRight.addEventListener('click', function(){
 })
 
 
+var swiper = new Swiper('.video-slider', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    }
+});
 
 
-
-const swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.brand-slider', {
   spaceBetween: 20,
   loop: true,
   autoplay: {
